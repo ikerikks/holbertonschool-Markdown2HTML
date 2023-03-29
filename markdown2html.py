5581ab8635a245552c74da9df5a7eb3e7a866598
ikerikks <ikerikks@gmail.com>
2023-03-29 16:47:02 +0200

    start a script

---
+++ markdown2html.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import os
+
+file = "README.md"
+
+if len(sys.argv) < 2:
+  print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+  exit(1)
+
+elif os.path.exists(file) == False:
+  print("Missing " + file  , file=sys.stderr)
+  exit(1)
+
+else:
+  exit(0)
