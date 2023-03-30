e5e4242d2cf56750894286d34a2b2d38910f07b1
ikerikks <ikerikks@gmail.com>
2023-03-30 11:27:53 +0200

    update of code

--- markdown2html.py
+++ markdown2html.py
@@ -4,9 +4,9 @@ import sys
 import os
 
 if __name__ == "__main__":
-    file = "README.md"
+    file = sys.argv[1]
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
 
