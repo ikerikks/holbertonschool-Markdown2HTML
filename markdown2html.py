5fcce3d9ef207a2496cb156b1bb5fe13f5968563
ikerikks <ikerikks@gmail.com>
2023-03-30 13:23:52 +0200

    adding script to generate HTML

--- markdown2html.py
+++ markdown2html.py
@@ -2,6 +2,7 @@
 '''Python Script'''
 import sys
 import os
+import markdown
 
 if __name__ == "__main__":
 
@@ -14,4 +15,13 @@ if __name__ == "__main__":
         exit(1)
 
     else:
+        src_file = open(sys.argv[1], "r")
+        dest_file = open(sys.argv[2], "w")
+    
+        #html conversion
+        hmtl = markdown.markdown(src_file.read())
+        #send the content to the new file
+        dest_file.write(hmtl + '\n')
+        src_file.close()
+        dest_file.close()
         exit(0)
