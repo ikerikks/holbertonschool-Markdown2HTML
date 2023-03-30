c18969fa4ce47eb95e16c0c128f48f009be3c860
ikerikks <ikerikks@gmail.com>
2023-03-30 11:19:41 +0200

    adding new condition

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-#Python Script
+'''Python Script'''
 import sys
 import os
 
-file = "README.md"
+if __name__ == "__main__":
+    file = "README.md"
 
-if len(sys.argv) < 2:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
 
-elif os.path.exists(file) is False:
-    print("Missing " + file, file=sys.stderr)
-    exit(1)
+    elif os.path.exists(file) is False:
+        print("Missing " + file, file=sys.stderr)
+        exit(1)
 
-else:
-    exit(0)
+    else:
+        exit(0)
