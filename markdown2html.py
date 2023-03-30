08ce0f18efdecceb2bc653728221cdb74632f038
ikerikks <ikerikks@gmail.com>
2023-03-30 11:32:18 +0200

    supression of variable

--- markdown2html.py
+++ markdown2html.py
@@ -4,14 +4,13 @@ import sys
 import os
 
 if __name__ == "__main__":
-    file = sys.argv[1]
 
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
 
-    elif os.path.exists(file) is False:
-        print("Missing " + file, file=sys.stderr)
+    elif os.path.exists(sys.argv[1]) is False:
+        print("Missing " + sys.argv[1], file=sys.stderr)
         exit(1)
 
     else:
