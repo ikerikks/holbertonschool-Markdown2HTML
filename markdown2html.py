9bf0e2f13c37b4472ea10b6bdeac6a7c78334822
ikerikks <ikerikks@gmail.com>
2023-04-05 10:17:39 +0200

    nested code in a function

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-This script converts Markdown syntax to HTML format.
+Converts Markdown files to HTML format.
 
 Usage: ./markdown2html.py README.md README.html
 '''
@@ -9,24 +9,29 @@ import sys
 import os
 import markdown
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
-elif os.path.exists(sys.argv[1]) is False:
-    print("Missing " + sys.argv[1], file=sys.stderr)
-    exit(1)
-else:
-    # Open input and output files
-    src_file = open(sys.argv[1], "r")
-    dest_file = open(sys.argv[2], "w")
+def main():
+    '''Main function to run the script.'''
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    elif os.path.exists(sys.argv[1]) is False:
+        print("Missing " + sys.argv[1], file=sys.stderr)
+        exit(1)
+    else:
+        # Open input and output files
+        src_file = open(sys.argv[1], "r")
+        dest_file = open(sys.argv[2], "w")
 
-    # Convert Markdown to HTML
-    hmtl = markdown.markdown(src_file.read())
+        # Convert Markdown to HTML
+        hmtl = markdown.markdown(src_file.read())
 
-    # Write HTML content to output file
-    dest_file.write(hmtl + '\n')
+        # Write HTML content to output file
+        dest_file.write(hmtl + '\n')
 
-    # Close files and exit
-    src_file.close()
-    dest_file.close()
-    exit(0)
+        # Close files and exit
+        src_file.close()
+        dest_file.close()
+        exit(0)
+
+if __name__ == '__main__':
+    main()
