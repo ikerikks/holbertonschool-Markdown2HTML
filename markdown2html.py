0ded8898ed03b7b8929dcdcf5d72750e87b2498e
ikerikks <ikerikks@gmail.com>
2023-04-05 12:29:01 +0200

    correction

--- markdown2html.py
+++ markdown2html.py
@@ -6,21 +6,23 @@ Usage: ./markdown2html.py README.md README.html
 '''
 
 import sys
+from sys import stderr, argv
 import os
 import markdown
 
+
 def main():
     '''Main function to run the script.'''
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    if len(argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
-    elif os.path.exists(sys.argv[1]) is False:
-        print("Missing " + sys.argv[1], file=sys.stderr)
+    elif os.path.exists(argv[1]) is False:
+        print("Missing " + argv[1], file=stderr)
         exit(1)
     else:
         # Open input and output files
-        src_file = open(sys.argv[1], "r")
-        dest_file = open(sys.argv[2], "w")
+        src_file = open(argv[1], "r")
+        dest_file = open(argv[2], "w")
 
         # Convert Markdown to HTML
         hmtl = markdown.markdown(src_file.read())
@@ -32,6 +34,7 @@ def main():
         src_file.close()
         dest_file.close()
         exit(0)
+
 
 if __name__ == '__main__':
     main()
